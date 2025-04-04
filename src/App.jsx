904f904a4364bf12753a0a4b26adcf0c1626512b
bittu-roy <bitturoy32001@gmail.com>
2025-04-04 11:48:58 +0530

    further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,69 @@
-function App() {
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
+function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              {...CORE_CONCEPTS[0]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
+      </main>
+    </div>
+  ); v
+}
+//function App-> 1. must start with Uppercase character
+//               2. must return renderable content
 export default App;
+
+//jsx-> stands for javascript extension used to write code in a declarative way
+// but this extension will not work in browsers
+//So behind the scene the build process in React transforms the code that works in a browser
+
+//PROPS- helps to pass data into components and use that components data.
