77c9b72761bb46a60b2b8514a70940462262e249
bittu-roy <bitturoy32001@gmail.com>
2025-04-07 14:58:10 +0530

    splitting components + Forwarding Props

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,12 @@
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
 
     // children props-> provided by React and contains the component we have in between the tabs.
     // which is Component
-    //<TabButton>Component</TabButton>
+    //<TabButton>Component</TabButton> from App.jsx
     //And this way where components can wrap other components is called component composition.
     return (
         <li>
-            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+            <button className={isSelected ? 'active' : undefined} {...props}>
                 {children}
             </button>
         </li>
