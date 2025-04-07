1dfe10dbe10a052dfeb6647a7c20d3e53b55d1e2
bittu-roy <bitturoy32001@gmail.com>
2025-04-07 10:31:12 +0530

    Rendering Content Dynamically + Dynamic Styling + Outputting List data Dynamically

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,12 +1,14 @@
-export default function TabButton({ children, onSelect }) {
-  
+export default function TabButton({ children, onSelect, isSelected }) {
+
     // children props-> provided by React and contains the component we have in between the tabs.
     // which is Component
     //<TabButton>Component</TabButton>
     //And this way where components can wrap other components is called component composition.
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+                {children}
+            </button>
         </li>
     );
 } 
